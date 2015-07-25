c0adf04dfe4f756568f6ade878f21794e357da57
Han Chang <szu.han.chang@gmail.com>
2015-07-24 23:50:04 -0400

    Support both types of Google results

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var google = require('./lib/google')
 
-google('Microsoft', 'us', 'en', function (err, next, links) {
+google('weekly meal planner', 'us', 'en', function (err, next, links) {
   console.log(links)
 })
