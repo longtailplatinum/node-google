46b3f8ef79bdb2cc118f77beb4fcae6d77b7542b
NodeXperts16 <nodeexperts@gmail.com>
2017-10-03 13:41:35 +0530

    Added support for cert

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var google = require('./lib/google')
 
-google('paintings', 'in', 'en', 0, null, function (err, next, links) {
+google('paintings', 'in', 'en', 0, null, cert, function (err, next, links) {
   console.log(links)
 })
